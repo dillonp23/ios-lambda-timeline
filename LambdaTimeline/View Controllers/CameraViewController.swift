4d42a2834669ef14ffe4a93f6130611dec2ff989
dillonp23 <dillonp.lambda@gmail.com>
2020-03-21 16:49:07 -0700

    Added ability to record a video for a new post. Playback movie once recording ends. Add gesture recognizer to replay video upon tapping the screen. WIP: adjust size of playback view

--- LambdaTimeline/View Controllers/CameraViewController.swift
+++ LambdaTimeline/View Controllers/CameraViewController.swift
@@ -26,6 +26,15 @@ class CameraViewController: UIViewController {
         
         cameraView.videoPlayerLayer.videoGravity = .resizeAspectFill
         setUpCamera()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture(_:)))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func handleTapGesture(_ tapGesture: UITapGestureRecognizer) {
+        if tapGesture.state == .ended {
+            replayRecording()
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -36,6 +45,10 @@ class CameraViewController: UIViewController {
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         captureSession.stopRunning()
+    }
+    
+    private func updateViews() {
+        recordButton.isSelected = fileOutput.isRecording
     }
     
     // MARK: - Camera & Microphone Set Up Methods
@@ -102,9 +115,92 @@ class CameraViewController: UIViewController {
     // MARK: - Button Actions
     
     @IBAction func saveButtonTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Add a title", message: "Please enter a title for your video post", preferredStyle: .alert)
+        
+        alert.addTextField(configurationHandler: nil)
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        let addPostAction = UIAlertAction(title: "Confirm", style: .default) { (_) in
+            guard let postController = self.postController, let title = alert.textFields?.first?.text else { return }
+            print(postController)
+            print(title)
+            
+//            postController.createPost(with: title, ofType: .video, mediaData: <#T##Data#>, ratio: <#T##CGFloat?#>, completion: <#T##(Bool) -> Void#>)
+        }
+        
+        alert.addAction(cancel)
+        alert.addAction(addPostAction)
+        self.present(alert, animated: true)
     }
     
     @IBAction func recordButtonTapped(_ sender: Any) {
+        toggleRecording()
     }
+    
+    //MARK: - Video Recording Functions
+    
+    private func toggleRecording() {
+        if fileOutput.isRecording {
+            fileOutput.stopRecording()
+        } else {
+            fileOutput.startRecording(to: newRecordingURL(), recordingDelegate: self)
+        }
+    }
+    
+    private func newRecordingURL() -> URL {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withInternetDateTime]
+
+        let name = formatter.string(from: Date())
+        let fileURL = documentsDirectory.appendingPathComponent(name).appendingPathExtension("mov")
+        return fileURL
+    }
+    
+    // MARK: - Video Playback Functions
+    
+    private func replayRecording() {
+        if let player = player {
+            player.seek(to: CMTime.zero)
+            player.play()
+        }
+    }
+    
+    private func playMovie(url: URL) {
+        player = AVPlayer(url: url)
+        
+        let playerLayer = AVPlayerLayer(player: player)
+        
+        let playbackView = view.bounds
+        
+        playerLayer.frame = playbackView
+        view.layer.addSublayer(playerLayer)
+        
+        player.play()
+    }
+    
+}
+
+
+extension CameraViewController: AVCaptureFileOutputRecordingDelegate {
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+        updateViews()
+    }
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        if let error = error {
+            print("Error saving video to file output: \(error)")
+        }
+        
+        print("Video url: \(outputFileURL)")
+        
+        updateViews()
+        
+        playMovie(url: outputFileURL)
+    }
+    
     
 }
