3c177e590696207db7a90aea155e200d47c70110
dillonp23 <dillonp.lambda@gmail.com>
2020-03-21 13:05:59 -0700

    Added UI for camera view to allow for adding video post. Added custom CameraView and CameraViewController

---
+++ LambdaTimeline/View Controllers/CameraViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CameraViewController.swift
+//  LambdaTimeline
+//
+//  Created by Dillon P on 3/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
---
+++ LambdaTimeline/Views/CameraView.swift
@@ -0,0 +1,27 @@
+//
+//  CameraView.swift
+//  LambdaTimeline
+//
+//  Created by Dillon P on 3/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraView: UIView {
+
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+    
+    var videoPlayerLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+    
+    var session: AVCaptureSession? {
+        get { return videoPlayerLayer.session }
+        set { videoPlayerLayer.session = newValue }
+    }
+
+}
